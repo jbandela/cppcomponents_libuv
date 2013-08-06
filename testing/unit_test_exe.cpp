c3385b44f9f0fc70cfe17ad0638b2651757903ea
John Bandela <jbandela@gmail.com>
2013-08-06 11:59:10 -0400

    Simple future test working in mingw (from mingw-builds)

--- testing/unit_test_exe.cpp
+++ testing/unit_test_exe.cpp
@@ -1,11 +1,29 @@
 #pragma comment(lib, "Ws2_32.lib")
 #pragma comment(lib, "Iphlpapi.lib")
 #pragma comment(lib, "Psapi.lib")
+#include <uv.h>
+
+#ifdef _MSC_VER
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+namespace{
+	struct MemLeakCheckInit{
+		MemLeakCheckInit(){
+			_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+		}
+	};
+
+	MemLeakCheckInit mlcinit;
+}
+
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <atomic>
+#include <thread>
 
-#include <uv.h>
 #include <thread>
 #include <chrono>
 #include <type_traits>
@@ -20,24 +38,147 @@
 
 void print_tid(const std::string& str  = "On Thread "){
 	std::stringstream s;
-	s << str << " " << std::this_thread::get_id() << "\n";
+	s << str << " " << uv_thread_self() << "\n";
 
 	fputs(s.str().c_str(),stderr);
 }
 
 template<class T>
-struct work:std::enable_shared_from_this<work<T>>{
+struct storage_and_error{
+	cppcomponents::error_code error_;
+
+	typename std::aligned_storage<sizeof(T), std::alignment_of<T>::value>::type storage_;
+	std::atomic<bool> storage_initialized_;
+	std::atomic<bool> finished_;
+
+
+
+
+
+	storage_and_error():error_(0),storage_initialized_(false),finished_(false){
+
+	}
+
+	bool finished()const{
+		return finished_.load();
+	}
+	
+	template<class F>
+	void do_assign(F& f){
+		try{
+			void* data = &storage_;
+			new(data) T(f());
+			storage_initialized_.store(true);
+
+		}
+		catch (std::exception& e){
+			error_ = cppcomponents::error_mapper::error_code_from_exception(e);
+		}
+		finished_.store(true);
+	}
+
+	void check_get()const {
+		if (!finished_.load()){
+			throw std::runtime_error("Operation not finished");
+		}
+		cppcomponents::throw_if_error(error_);
+		if (!storage_initialized_.load()){
+			throw std::runtime_error("Storage not initialized");
+		}
+	}
+
+	T& get(){
+		check_get();
+
+		void* data = &storage_;
+		auto& ret = *static_cast<T*>(data);
+		return ret;
+	}
+	const T& get()const{
+		check_get();
+		const void* data = &storage_;
+		return *static_cast<const T*>(data);
+	}
+
+	~storage_and_error(){
+		if (storage_initialized_.load()){
+			void* data = &storage_;
+			static_cast<const T*>(data)->~T();
+		}
+	}
+};
+
+template<>
+struct storage_and_error<void>{
+	cppcomponents::error_code error_;
+
+	std::atomic<bool> finished_;
+
+
+
+
+
+	storage_and_error():error_(0), finished_(false){
+
+	}
+
+	bool finished()const{
+		return finished_.load();
+	}
+
+	template<class F>
+	void do_assign(F& f){
+		try{
+			f();
+
+		}
+		catch (std::exception& e){
+			error_ = cppcomponents::error_mapper::error_code_from_exception(e);
+		}
+		finished_.store(true);
+	}
+
+	void check_get()const{
+		if (!finished_.load()){
+			throw std::runtime_error("Operation not finished");
+		}
+		cppcomponents::throw_if_error(error_);
+
+	}
+
+	void get()const{
+		check_get();
+	}
+
+
+	~storage_and_error(){
+
+	}
+};
+
+template<class D, class T>
+struct work_move{
+	T && move(){
+		return std::move(static_cast<D*>(this)->storage_.get());
+	}
+};
+
+template<class D>
+struct work_move<D,void>{
+
+};
+
+template<class T>
+struct work:std::enable_shared_from_this<work<T>>,work_move<work<T>,T>{
 
 	uv_work_t w_;
 	std::atomic_flag then_called_;
-	std::atomic<bool> finished_;
 	std::atomic_flag has_then_;
 	std::function < void()> then_;
 	std::function < T()> worker;
-	std::atomic<bool> storage_initialized_;
-	cppcomponents::error_code error_;
 
-	typename std::aligned_storage<sizeof(T),std::alignment_of<T>::value>::type storage_;
+
+	storage_and_error<T> storage_;
 
 	std::shared_ptr<work> shared_self_;
 
@@ -47,7 +188,7 @@ struct work:std::enable_shared_from_this<work<T>>{
 	template<class F>
 	work(F f)
 		: w_{}, then_called_{ ATOMIC_FLAG_INIT },
-		finished_{ false }, has_then_{ ATOMIC_FLAG_INIT }, worker(f), error_(0), storage_initialized_{false}, then_added_(false)
+		has_then_{ ATOMIC_FLAG_INIT }, worker(f),  then_added_(false)
 	{
 		w_.data = this;
 	}
@@ -62,28 +203,11 @@ struct work:std::enable_shared_from_this<work<T>>{
 	}
 
 
-	void check_get(){
-		if (!finished_.load()){
-			throw std::runtime_error("Operation not finished");
-		}
-		cppcomponents::throw_if_error(error_);
-		if (!storage_initialized_.load()){
-			throw std::runtime_error("Storage not initialized");
-		}
+	T get()const{
+		return storage_.get();
 	}
 
-	T& get(){
-		check_get(); 
 
-		void* data = &storage_;
-		auto& ret = *static_cast<T*>(data);
-		return ret;
-	}
-	const T& get()const{
-		check_get();
-		const void* data = &storage_;
-		return *static_cast<const T*>(data);
-	}
 
 	template<class TF>
 	auto then(bool use_default_loop,TF f)->std::shared_ptr<work<decltype(f(shared_self_))>>{
@@ -106,7 +230,7 @@ struct work:std::enable_shared_from_this<work<T>>{
 			}
 		};
 		has_then_.test_and_set();
-		if (finished_.load()){
+		if (storage_.finished()){
 			if (then_called_.test_and_set() == false){
 				then_();
 				then_ = [](){};
@@ -122,16 +246,8 @@ struct work:std::enable_shared_from_this<work<T>>{
 	}
 	static void do_work(uv_work_t* wt){
 		auto& w = *static_cast<work*>(wt->data);
-		void* data = &w.storage_;
-		try{
-			new(data) T(w.worker());
-			w.storage_initialized_.store(true);
+		w.storage_.do_assign(w.worker);
 
-		}
-		catch (std::exception& e){
-			w.error_ = cppcomponents::error_mapper::error_code_from_exception(e);
-		}
-		w.finished_.store(true);
 		if (w.has_then_.test_and_set()){
 			if (w.then_called_.test_and_set()==false){
 				w.then_();		
@@ -159,12 +275,53 @@ struct work:std::enable_shared_from_this<work<T>>{
 
 
 	~work(){
-		if (storage_initialized_.load()){
-			void* data = &storage_;
-			static_cast<const T*>(data)->~T();
-		}
+
 	}
 };
+
+
+template<class D, class T>
+struct future_move{
+	T && move(){
+		return static_cast<D*>(this)->pw_->move();
+	}
+};
+
+template<class D>
+struct future_move<D, void>{
+
+};
+
+template<class T>
+struct future:future_move<future<T>,T>{
+	typedef work<T> w_t;
+	std::shared_ptr<w_t> pw_;
+
+	template<class F>
+	future(F f) : pw_(std::make_shared<w_t>(f)){
+		pw_->start();
+	}
+
+	future(std::shared_ptr < work < T >> p) : pw_(p){}
+
+	template<class F>
+	auto then(bool run_on_default_loop, F f)->future < decltype(f(future(pw_))) > {
+		return pw_->then(run_on_default_loop, [f](std::shared_ptr<w_t> w){
+			return f(future(w));
+		});
+	}
+	template<class F>
+	auto then(F f)->future < decltype(f(future(pw_))) > {
+		return this->then(false, f);
+	}
+
+	T get()const {
+		return pw_->get();
+	}
+
+
+};
+
 
 
 long fib_(long t) {
@@ -175,10 +332,7 @@ long fib_(long t) {
 }
 
 long fib(int n) {
-	if (rand() % 2)
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
-	else
-		std::this_thread::sleep_for(std::chrono::milliseconds(3));
+
 	long f = fib_(n);
 	return f;
 }
@@ -199,6 +353,13 @@ int main() {
 		return 0;
 	});
 
+	future<long> fut([](){return fib(15); });
+	fut.then([](future<long> fu){
+		auto f = fu.get();
+		fprintf(stderr, "%dth fibonacci is %lu\n", 15, f);
+		print_tid("In then");
+
+	});
 
 	return uv_run(uv_default_loop(), UV_RUN_DEFAULT);
 }
