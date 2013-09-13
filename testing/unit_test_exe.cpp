ee756c71b3d1897d6f70a3665dca3e35ab10102c
John Bandela <jbandela@gmail.com>
2013-09-13 09:38:00 -0400

    Removed need for executor parameter

--- testing/unit_test_exe.cpp
+++ testing/unit_test_exe.cpp
@@ -37,17 +37,6 @@ int main() {
 	{
 		auto loop = luv::Loop::DefaultLoop();
 
-		cppcomponents::LoopExecutor executor;
-		luv::Prepare prep{ loop };
-
-		prep.Start([&executor](use<luv::IPrepare>, int status){
-
-			executor.RunQueuedClosures();
-		});
-
-
-
-
 		luv::TcpStream server{ loop };
 
 		auto bind_addr = luv::Uv::Ip4Addr("0.0.0.0", 7001);
@@ -63,7 +52,7 @@ int main() {
 
 			std::string data;
 			while (true){
-				auto f = await.as_future(executor, chan.Read());
+				auto f = await.as_future(chan.Read());
 				auto errcode = f.ErrorCode();
 				if (errcode < 0){
 					break;
@@ -82,7 +71,7 @@ another-footer: another-value
 
 abcdefghijklmnopqrstuvwxyz1234567890abcdef
 )";
-				await(executor, client.Write(response));
+				await( client.Write(response));
 
 			}
 			chan.Close();
@@ -93,8 +82,6 @@ abcdefghijklmnopqrstuvwxyz1234567890abcdef
 			auto get_object_count = m.load_module_function<std::size_t(*)()>("GetObjectCount");
 			auto count = get_object_count();
 			std::cerr << "\nObject count = " << count << "\n\n";
-			//await(executor,server.Close());
-
 			loop.Stop();
 		}));
 
@@ -103,29 +90,26 @@ abcdefghijklmnopqrstuvwxyz1234567890abcdef
 			auto bind_addr = luv::Uv::Ip4Addr("127.0.0.1", 7001);
 			luv::TcpStream client{ loop };
 
-			await(executor, client.Connect(bind_addr));
+			await( client.Connect(bind_addr));
 			auto reader = client.GetReadChannel();
 			cppcomponents::unique_channel<cppcomponents::use<cppcomponents::IBuffer>> uchan{ reader };
 			for (int i = 0; i < 15; i++){
-				await(executor, client.Write(cppcomponents::cr_string("Hello")));
+				await( client.Write(cppcomponents::cr_string("Hello")));
 				auto buf = await(reader.Read());
 				std::string s{ buf.Begin(), buf.End() };
 
 			}
 
 			reader.Close();
-			await(executor, client.Close());
+			await(client.Close());
 
 
 		});
 		for (int i = 0; i < 2; i++){
 			clientfunc();
 			loop.Run();
-			while (executor.NumPendingClosures()){
 				loop.RunNoWait();
-			}
 		}
-		prep.Stop();
 		server.ClearListener();
 	}
 	return 0;
