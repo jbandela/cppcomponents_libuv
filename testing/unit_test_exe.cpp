745bb48c380ae9bca80915c0b0c87313fb297411
John Bandela <jbandela@gmail.com>
2013-09-20 09:21:38 -0400

    Switched to Google test for testing

--- testing/unit_test_exe.cpp
+++ testing/unit_test_exe.cpp
@@ -8,7 +8,7 @@ namespace{
 	struct MemLeakCheckInit{
 		MemLeakCheckInit(){
 			_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-			//_crtBreakAlloc = 1654;
+			//_crtBreakAlloc = 579;
 		}
 	};
 
@@ -20,10 +20,12 @@ namespace{
 #include <mutex>
 #include <thread>
 #include "../cppcomponents_libuv/cppcomponents_libuv.hpp"
+#define PPL_HELPER_OUTPUT_ENTER_EXIT
 #include <cppcomponents_async_coroutine_wrapper/cppcomponents_resumable_await.hpp>
 
-#define CATCH_CONFIG_MAIN  
-#include "external/catch.hpp"
+#include <gtest/gtest.h>
+
+
 
 namespace luv = cppcomponents_libuv;
 using cppcomponents::use;
@@ -31,7 +33,7 @@ using cppcomponents::use;
 const int TEST_PORT = 7171;
 
 
-TEST_CASE("test-async", "test-async"){
+TEST(async,async){
 		int prepare_cb_called = 0;
 	int async_cb_called = 0;
 	int close_cb_called = 0;
@@ -40,7 +42,7 @@ TEST_CASE("test-async", "test-async"){
 	std::mutex mutex;
 	mutex.lock();
 
-	auto loop = luv::Loop::DefaultLoop();
+	luv::Loop loop{};
 	luv::Prepare prepare{ loop };
 
 
@@ -49,7 +51,7 @@ TEST_CASE("test-async", "test-async"){
 	};
 
 	auto async_cb = [&](use<luv::IAsync> async, int status){
-		REQUIRE(status == 0);
+		EXPECT_EQ(status,0);
 
 		mutex.lock();
 		int n = ++async_cb_called;
@@ -87,7 +89,7 @@ TEST_CASE("test-async", "test-async"){
 
 
 	auto prepare_cb = [&](use<luv::IPrepare> p, int status){
-		REQUIRE(status == 0);
+		EXPECT_EQ(status , 0);
 		if (prepare_cb_called++)
 			return;
 
@@ -101,10 +103,10 @@ TEST_CASE("test-async", "test-async"){
 
 	loop.Run();
 
-	REQUIRE(prepare_cb_called > 0);
+	EXPECT_GT(prepare_cb_called , 0);
 
-	REQUIRE(async_cb_called == 3);
-	REQUIRE(close_cb_called == 2);
+	EXPECT_EQ(async_cb_called , 3);
+	EXPECT_EQ(close_cb_called , 2);
 
 	thread.join();
 
@@ -112,11 +114,10 @@ TEST_CASE("test-async", "test-async"){
 
 	
 
-
 }
 
 
-TEST_CASE("test-connection-fail", "test-connection-fail"){
+TEST(connection_fail,connection_fail){
 
 	auto loop = luv::Loop::DefaultLoop();
 	use<luv::ITcpStream> tcp;
@@ -137,11 +138,11 @@ TEST_CASE("test-connection-fail", "test-connection-fail"){
 	};
 
 	auto timer_cb = [&](use<luv::ITimer>, int status){
-		REQUIRE(status == 0);
+		EXPECT_EQ(status , 0);
 		timer_cb_calls++;
 
-		REQUIRE(close_cb_calls == 0);
-		REQUIRE(connect_cb_calls == 1);
+		EXPECT_EQ(close_cb_calls , 0);
+		EXPECT_EQ(connect_cb_calls , 1);
 
 		tcp.Close().Then(on_close);
 		timer.Close().Then(timer_close_cb);
@@ -149,19 +150,19 @@ TEST_CASE("test-connection-fail", "test-connection-fail"){
 
 	auto on_connect_with_close = [&](cppcomponents::Future<int> fut) {
 		auto status = fut.ErrorCode();
-		REQUIRE(status == luv::ErrorCodes::Connrefused);
+		EXPECT_EQ(status , luv::ErrorCodes::Connrefused);
 		connect_cb_calls++;
 
-		REQUIRE(close_cb_calls == 0);
+		EXPECT_EQ(close_cb_calls , 0);
 		tcp.Close().Then(on_close);
 	};
 
 	auto on_connect_without_close = [&](cppcomponents::Future<int> fut){
 		auto status = fut.ErrorCode();
-		REQUIRE(status == luv::ErrorCodes::Connrefused);
+		EXPECT_EQ(status , luv::ErrorCodes::Connrefused);
 		connect_cb_calls++;
 		timer.Start(timer_cb, 100, 0);
-		REQUIRE(close_cb_calls == 0);
+		EXPECT_EQ(close_cb_calls , 0);
 	};
 
 	auto connection_fail = 
@@ -178,16 +179,16 @@ TEST_CASE("test-connection-fail", "test-connection-fail"){
 
 			loop.Run();
 
-			REQUIRE(connect_cb_calls == 1);
-			REQUIRE(close_cb_calls == 1);
+			EXPECT_EQ(connect_cb_calls , 1);
+			EXPECT_EQ(close_cb_calls , 1);
 
 	};
 
 	auto test_impl_connection_fail = [&](){
 		connection_fail(on_connect_with_close);
 
-		REQUIRE(timer_close_cb_calls == 0);
-		REQUIRE(timer_cb_calls == 0);
+		EXPECT_EQ(timer_close_cb_calls , 0);
+		EXPECT_EQ(timer_cb_calls , 0);
 	};
 
 	auto test_impl_connection_fail_doesnt_auto_close = [&]() {
@@ -196,8 +197,8 @@ TEST_CASE("test-connection-fail", "test-connection-fail"){
 
 		connection_fail(on_connect_without_close);
 
-		REQUIRE(timer_close_cb_calls == 1);
-		REQUIRE(timer_cb_calls == 1);
+		EXPECT_EQ(timer_close_cb_calls , 1);
+		EXPECT_EQ(timer_cb_calls , 1);
 
 	};
 
@@ -212,10 +213,10 @@ TEST_CASE("test-connection-fail", "test-connection-fail"){
 	timer_cb_calls = 0;
 	test_impl_connection_fail_doesnt_auto_close();
 
+
 }
 
-TEST_CASE("TCP", "TcpStream"){
-
+TEST(Tcp,TcpStream){
 	auto loop = luv::Loop{};
 
 
@@ -243,7 +244,7 @@ TEST_CASE("TCP", "TcpStream"){
 				}
 				auto buf = fut.Get();
 				std::string s{ buf.Begin(), buf.End() };
-				REQUIRE(s == "Hello");
+				EXPECT_EQ(s , "Hello");
 				std::stringstream strstream;
 				strstream << "Hi " << k++;
 				std::string response = strstream.str();
@@ -261,11 +262,11 @@ TEST_CASE("TCP", "TcpStream"){
 				await(executor,client.Write(std::string("Hello")));
 				auto buf = await(executor,chan.Read());
 				std::string response{ buf.Begin(), buf.End() };
-				REQUIRE(std::string("Hi 0") == response);
+				EXPECT_EQ(std::string("Hi 0") , response);
 				await(client.Write(std::string("Hello")));
 				buf = await(executor,chan.Read());
 				response.assign( buf.Begin(), buf.End() );
-				REQUIRE(std::string("Hi 1") == response);
+				EXPECT_EQ(std::string("Hi 1") , response);
 			}
 
 
@@ -278,16 +279,15 @@ TEST_CASE("TCP", "TcpStream"){
 		client_func();
 	loop.Run();
 
-
 	
-
 } 
+
 
 
 #include <fcntl.h>
 #include <sys/stat.h>
 
-TEST_CASE("fs", "fs1"){
+TEST(fs,fs1){
 
 	luv::Loop loop;
 
@@ -301,76 +301,76 @@ TEST_CASE("fs", "fs1"){
 		std::string filename = "./testdir/testfile.txt";
 
 		// Create a directory
-		REQUIRE(0 == await.as_future(file.Mkdir("./testdir", S_IWRITE|S_IREAD|S_IFDIR|S_IEXEC)).ErrorCode());
+		EXPECT_EQ(0 , await.as_future(file.Mkdir("./testdir", S_IWRITE|S_IREAD|S_IFDIR|S_IEXEC)).ErrorCode());
 
 		// Create a file in the directory
-		REQUIRE(0 == await.as_future(file.Open(filename, O_CREAT | O_RDWR, S_IWRITE|S_IREAD)).ErrorCode());
+		EXPECT_EQ(0 , await.as_future(file.Open(filename, O_CREAT | O_RDWR, S_IWRITE|S_IREAD)).ErrorCode());
 
 
 		// Write some data
 		std::string out = "Hello World";
-		REQUIRE(0 == await.as_future(file.Write(out.data(), out.size(), 0)).ErrorCode());
+		EXPECT_EQ(0 , await.as_future(file.Write(out.data(), out.size(), 0)).ErrorCode());
 
 		// Sync and close
-		REQUIRE(0 == await.as_future(file.Sync()).ErrorCode());
-		REQUIRE(0 == await.as_future(file.Close()).ErrorCode());
+		EXPECT_EQ(0 , await.as_future(file.Sync()).ErrorCode());
+		EXPECT_EQ(0 , await.as_future(file.Close()).ErrorCode());
 
 		// Open it again
-		REQUIRE(0 == await.as_future(file.Open(filename, O_RDONLY, 0)).ErrorCode());
+		EXPECT_EQ(0 , await.as_future(file.Open(filename, O_RDONLY, 0)).ErrorCode());
 		std::vector<char> buf(100);
 
 		// Read from it
 		auto sz = await(file.Read(&buf[0], buf.size(), 0));
 		std::string in{ buf.begin(), buf.begin() + sz };
-		REQUIRE(out == in);
+		EXPECT_EQ(out , in);
 
-		REQUIRE(0 == await.as_future(file.Close()).ErrorCode());
+		EXPECT_EQ(0 , await.as_future(file.Close()).ErrorCode());
 
-		REQUIRE(0 == await.as_future(file.Open(filename, O_WRONLY, 0)).ErrorCode());
+		EXPECT_EQ(0 , await.as_future(file.Open(filename, O_WRONLY, 0)).ErrorCode());
 
 		// Truncate it
-		REQUIRE(0 == await.as_future(file.Truncate(5)).ErrorCode());
+		EXPECT_EQ(0 , await.as_future(file.Truncate(5)).ErrorCode());
 
 		// Close it
-		REQUIRE(0 == await.as_future(file.Close()).ErrorCode());
+		EXPECT_EQ(0 , await.as_future(file.Close()).ErrorCode());
 
 		// Open it again
-		REQUIRE(0 == await.as_future(file.Open(filename, O_RDONLY, 0)).ErrorCode());
+		EXPECT_EQ(0 , await.as_future(file.Open(filename, O_RDONLY, 0)).ErrorCode());
 
 		// Check out truncated read
 		sz = await(file.Read(&buf[0], buf.size(), 0));
 		in.assign(buf.begin(), buf.begin() + sz);
-		REQUIRE(in == "Hello");
-		REQUIRE(0 == await.as_future(file.Close()).ErrorCode());
+		EXPECT_EQ(in , "Hello");
+		EXPECT_EQ(0 , await.as_future(file.Close()).ErrorCode());
 
 		// Stat it
 		auto stat = await(file.Stat(filename));
-		REQUIRE(stat.st_size == 5);
+		EXPECT_EQ(stat.st_size , 5);
 
 		// Check that it is in the directory
 		auto dirfiles = await(file.Readdir(dir,0));
-		REQUIRE(dirfiles.size() == 1);
-		REQUIRE(dirfiles[0] == "testfile.txt");
+		EXPECT_EQ(dirfiles.size() , 1);
+		EXPECT_EQ(dirfiles[0] , "testfile.txt");
 
 		// Delete the file
-		REQUIRE(0 == await.as_future(file.Unlink(filename)).ErrorCode());
+		EXPECT_EQ(0 , await.as_future(file.Unlink(filename)).ErrorCode());
 
 		// Check that it is not in the directory
 		dirfiles = await(file.Readdir(dir, 0));
-		REQUIRE(dirfiles.size() == 0);
+		EXPECT_EQ(dirfiles.size() , 0);
 
 		// Check that the directory we created is present
 		dirfiles = await(file.Readdir("./",0));
 		auto iter = std::find(dirfiles.begin(), dirfiles.end(), std::string{ "testdir" });
-		REQUIRE(iter != dirfiles.end());
+		EXPECT_NE(iter , dirfiles.end());
 
 		// Delete the directory
-		REQUIRE(0 == await.as_future(file.Rmdir(dir)).ErrorCode());
+		EXPECT_EQ(0 , await.as_future(file.Rmdir(dir)).ErrorCode());
 
 		// Check that it is no longer present
 		dirfiles = await(file.Readdir("./",0));
 		iter = std::find(dirfiles.begin(), dirfiles.end(), std::string{ "testdir" });
-		REQUIRE(iter == dirfiles.end());
+		EXPECT_EQ(iter , dirfiles.end());
 
 
 	});
@@ -380,9 +380,10 @@ TEST_CASE("fs", "fs1"){
 	loop.Run();
 
 	fut.Get();
+
 }
 
-TEST_CASE("udp,udp"){
+TEST(udp,udp){
 
 	luv::Loop loop;
 
@@ -400,7 +401,7 @@ TEST_CASE("udp,udp"){
 			recv_socket.RecvStart([&](use<luv::IUdpStream> stream, std::intptr_t nread,
 				cppcomponents::use<cppcomponents::IBuffer> buf, luv::SockAddr addr, unsigned int flags){
 					std::string s{ buf.Begin(), buf.End() };
-					REQUIRE(s == "Hello UDP");
+					EXPECT_EQ(s , "Hello UDP");
 					stream.RecvStop();
 					result = s;
 			});
@@ -409,7 +410,7 @@ TEST_CASE("udp,udp"){
 			send_socket.SetBroadcast(1);
 
 			auto send_addr = luv::Uv::Ip4Addr("255.255.255.255", 7768);
-			REQUIRE(0 == await.as_future(send_socket.Send(std::string("Hello UDP"), send_addr)).ErrorCode());
+			EXPECT_EQ(0 , await.as_future(send_socket.Send(std::string("Hello UDP"), send_addr)).ErrorCode());
 
 
 
@@ -418,8 +419,8 @@ TEST_CASE("udp,udp"){
 		auto fut = func();
 
 		loop.Run();
-		REQUIRE(0 == fut.ErrorCode());
-		REQUIRE(result == "Hello UDP");
+		EXPECT_EQ(0 , fut.ErrorCode());
+		EXPECT_EQ(result , "Hello UDP");
 	}
 	loop.Run();
 
