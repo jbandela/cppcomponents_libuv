cc9ef4e4e8898f4d8ffdd955cf4f69e144590a08
John Bandela <jbandela@gmail.com>
2013-09-25 13:26:39 -0400

    Updated example to also include fibonacci

--- examples/example1/main.cpp
+++ examples/example1/main.cpp
@@ -4,9 +4,15 @@
 // For resumable and awaiter
 #include <cppcomponents_async_coroutine_wrapper/cppcomponents_resumable_await.hpp>
 
+// For stringstream
+#include <sstream>
+
+// For this_thread and sleep_for
+#include <thread>
+#include <chrono>
 
 // use<Interface> wraps a cppcomponents interface for use
-// (the reason this is needed is that you can also implement_interface<Interface> when implementing an interface
+// (the reason this is needed is that you can also implement_interface<Interface> when implementing an interface)
 using cppcomponents::use;
 
 // Future<T> is just a template alias for use<IFuture<T>>
@@ -17,6 +23,9 @@ using cppcomponents::awaiter;
 
 // Resumable wraps a function taking awaiter into a function that returns Future
 using cppcomponents::resumable;
+
+// Like a Go channel (a little bit)
+using cppcomponents::Channel;
 
 // libuv types, Interfaces start with I
 // Classes do not start with I and provide implementation of interfaces
@@ -34,21 +43,86 @@ using cppcomponents_libuv::LoopExiter;
 using cppcomponents_libuv::ErrorCodes;
 
 
+void fibonacci(std::uint16_t n, Channel < std::pair<std::uint16_t, std::uint64_t> > chan){
+	// First fibonacci is 0
+	// So go ahead and write that as our first value
+	auto fut = chan.Write({ 0, 0 });
+
+	// A vector to hold our intermediate calculations
+	std::vector<std::uint64_t> fibs(n+1);
+
+	
+	if (n == 0){
+		return;
+	}
+	if (n == 1){
+		chan.Write({ 1, 1 });
+		return;
+	}
+
+	// Set the 0th and 1st fibonacci numbers
+	fibs[0] = 0;
+	fibs[1] = 1;
+
+	// Calculate all the fibonacci numbers up to and including n
+	for (std::uint16_t i = 2; i <= n; ++i){
+		// Fibonacci forumula
+		fibs[i] = fibs[i - 2] + fibs[i - 1];
+
+		// fut (the Future from chan.Write, will become ready when somebody reads the channel
+		// that is a trigger that we need to write the latest value to the channel
+		// Also, if we are done calculating (i==n) we write the value to the channel
+		if (fut.Ready() || i==n){
+			fut = chan.Write({ i, fibs[i] });
+		}
+
+		// Slow us down so it looks like a long calculation, otherwise it runs too fast
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+	}
+
+	
+
+}
 void handle_input(use<ITty> in, use<ITty> out, use<ITty> err, awaiter<void> await){
 	// Write out the prompt
-	await(out.Write("Type Quit and press return to quit, any other text to have it echoed\n"));
+	await(out.Write("Type Quit and press return to quit,Fib to get the latest calculated fibonacci number, any other text to have it echoed\n"));
 
+	// Start the fibonacci writer
+
+	// First make a channel to communicate with the fibonacci calculating function
+	auto fibchan = cppcomponents::make_channel < std::pair<std::uint16_t, std::uint64_t> >();
+
+	// 93 is the largest fibonacci number that will fit in 64bits
+	const std::uint16_t fibonacci_n = 93;
+
+	// Uv::Async will run the function on the libuv threadpool
+	Uv::Async(std::bind(fibonacci, fibonacci_n, fibchan));
 
 	// Start reading and get it as a channel
 	auto chan = in.ReadStartWithChannel();
+	
+	// The value of the nth fibonacci number
+	std::uint16_t n = 0;
+	std::uint64_t value = 0;
 
 	// Loop until we break
 	while (true){
-		// Read the channel and if we have Quit, the break otherwise echo
+		// Read the channel and if we have Quit, then break, if Fib then output latest fibonacci (up to max 100 otherwise echo
 		auto buf = await(chan.Read());
 		std::string s(buf.Begin(),buf.End());
-		if (s.substr(0,4) == "Quit"){
+		if (s.substr(0, 4) == "Quit"){
 			break;
+		}
+		else if (s.substr(0, 3) == "Fib"){
+			std::stringstream s;
+
+			// If n==fibonacci_n, then the calculation is already finished
+			if (n < fibonacci_n){
+				std::tie(n, value) = await(fibchan.Read());
+			}
+
+			s << "Fib(" << n << ") = " << value << "\n";
+			await(out.Write(s.str()));
 		}
 		else{
 			await(out.Write("You typed " + s));
